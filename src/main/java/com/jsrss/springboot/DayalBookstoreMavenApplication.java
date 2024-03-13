654cbd4d2d6d19dd84d799a3beb06a049a950536
shanyalsd <shankardayal.sd@gmail.com>
2024-03-13 18:53:56 +0530

    Update 3 by Shankar

--- src/main/java/com/jsrss/springboot/DayalBookstoreMavenApplication.java
+++ src/main/java/com/jsrss/springboot/DayalBookstoreMavenApplication.java
@@ -17,6 +17,7 @@ static Logger log = LoggerFactory.getLogger(DayalBookstoreMavenApplication.class
 		log.warn("My Boot App - Main() begins");
 		log.info("Update 1 by Shankar");
 		log.info("Update 2 by Shankar");
+		log.info("Update 3 by Shankar");
 		SpringApplication.run(DayalBookstoreMavenApplication.class, args);
 		log.info("My Boot App - Main() ends");
 		log.debug("My Boot App - Main() ends");
